de0dd9baa37397a1d481c05faf1d2131b91f3d26
qq23550310 <13822230672@163.com>
2018-02-08 14:55:47 +0800

    frist commit (add login page)

--- py_student_demo1/urls.py
+++ py_student_demo1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from stu_demo1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('login', views.login),
 ]
